aa51fe93bf245261956d9748746b520a5ae4642d
Gabriel Gil <gabriel@gabrielgil.es>
2016-07-28 18:17:39 +0000

    Add directive

---
+++ angular-chrome-i18n_directive.js
@@ -0,0 +1,21 @@
+angular.module('angular-chrome-i18n.directive', [])
+
+
+/**
+ * @ngdoc directive
+ * @name i18n
+ *
+ * @description
+ * Replaces the content of the element with the message translation text
+ * from chrome.i18n API.
+ */
+.directive('i18n', function () {
+  return {
+    scope: {
+      i18n: '@'
+    },
+    link: function (scope, element) {
+      element.replaceWith(chrome.i18n.getMessage(scope.i18n));
+    }
+  };
+});
